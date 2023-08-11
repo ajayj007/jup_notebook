6a1abe3dab042e45586a4e84864ca8f742220f1d
ajayj007 <117555481+ajayj007@users.noreply.github.com>
2023-08-12 02:15:35 +0530

    Add files via upload

---
+++ tools for data sci ass.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# In[4]:
+
+
+print("In this notebook, Data Science Tools and Ecosystem are summarized.")
+
+
+# ## some popular languages that Data scientists use are :
+# 
+# 1.python
+# 2.R
+# 3.SQL
+# 4.SCALA
+# 
+
+# ## some of the commonly used libraries used by Data Scientists include :
+# 
+# 1.Tensorflow.
+# 2.Numpy.
+# 3.Scipy.
+# 4.Matplotlib. 
+
+# ## Data Science Tools :
+# 
+# 1. Apache spark
+# 2.Sas
+# 3.BigML
+
+# # Arithmetic Operation :
+
+# In[6]:
+
+
+a = 4
+b = 7
+print('mul:', a * b)
+
+
+# In[7]:
+
+
+print((3*4)+5)
+
+
+# ## min to hour
+# 
+
+# In[9]:
+
+
+print( 200/60,'hr')
+
+
+# # Objective :
+
+# ###  Jupyter Notebook is a web-based interactive computing environment that you can use for data analysis and collaborative coding. It allows the integration of code, text, and visualizations into a single document.
+
+# # Author : 
+# #### Ajay Jadhav
+
+# In[ ]:
+
+
+
+
